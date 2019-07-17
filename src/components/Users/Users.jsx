420e928944be412b37a7c15d89e8bf886ad104fb
viktr1432 <viktr666@gmail.com>
2019-07-17 19:39:01 +0300

    Update Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -34,8 +34,8 @@ let Users = (props) => {
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        {/* <div>{u.location.country}</div> */}
-                        {/* <div>{u.location.city}</div> */}
+                        <div>{/*u.location.country*/}</div>
+                    <div>{/*u.location.city*/}</div>
                     </span>
                 </span>
             </div>)
