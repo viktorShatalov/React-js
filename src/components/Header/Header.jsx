06db49929c87229c5efa50b5b793a8acee8cbadd
viktr1432 <viktr666@gmail.com>
2019-07-20 17:48:47 +0300

    Both conflict
    пока не понял что произошло но разберусь!!!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,16 @@ const Header =(props) => {
       </div>
     </header>;
   }
-
-
-
+const  Header = () =>{
+    return  <header className = {s.header}>
+    <div className={s.header__logo}>
+    <img src = 'https://www.freelogodesign.org/Content/img/logo-ex-7.png'/>
+    </div>
+    <div>
+      <div className={s.header__search}>
+        
+      </div>
+    </div>
+  </header>;
+}
 export default Header;
