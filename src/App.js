43830c9ac914ee9e29863a0699675949e14bebf8
viktr1432 <viktr666@gmail.com>
2019-07-23 18:40:28 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -20,22 +20,20 @@ const App = (props) => {
         <Navbar />
         <div className='app-wrapper-content'>
           
-          <Route path='/dialogs'
-            render={() => <DialogsConteiner  />} />
+          <Route path='/dialogs'render={() => <DialogsConteiner  />} />
 
-          <Route path='/profile/:userId?'
-            render={() => <ProfileContainer />} />
+          <Route path='/profile' render={() => <ProfileContainer />} />
 
-          <Route path='/users'
-            render={() => <UsersContainer />} />
+          <Route path='/users' render={() => <UsersContainer />} />
            
-          <Route path='/login'
-            render={() => <Login />} />
+          <Route path='/login'render={() => <Login />} />
 
           <Route path='/settings' render={() => <Settings />} />
-          <Route path='/music' render={() => <Music />} />
-          <Route path='/news' render={() => <News />} />
 
+          <Route path='/music' render={() => <Music />} />
+
+          <Route path='/news' render={() => <News />} />
+          
         </div>
       </div>
   );
--- src/Redux/auth-reduser.js
+++ src/Redux/auth-reduser.js
@@ -13,13 +13,11 @@ let initialState = {
 const authReduser = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER_DATA:
-
             return {
                 ...state,
                 ...action.data,
                 isAuth: true
             }
-
         default:
             return state;
     }
