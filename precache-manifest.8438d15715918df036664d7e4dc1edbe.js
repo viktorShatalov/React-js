934cf28fd1166436490e5a4e92b92c3438230122
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2019-10-03 12:26:29 +0300

    Updates

---
+++ precache-manifest.8438d15715918df036664d7e4dc1edbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bad2788dc7479ab0bebdee2e2335b37",
+    "url": "/React-js/index.html"
+  },
+  {
+    "revision": "8024a3123073ade33322",
+    "url": "/React-js/static/css/main.eb5ab785.chunk.css"
+  },
+  {
+    "revision": "0ae6bda4ee51a9f43f14",
+    "url": "/React-js/static/js/2.ea483479.chunk.js"
+  },
+  {
+    "revision": "8024a3123073ade33322",
+    "url": "/React-js/static/js/main.e1de3c47.chunk.js"
+  },
+  {
+    "revision": "a1a74a00c92fa0a58b2b",
+    "url": "/React-js/static/js/runtime~main.9b822775.js"
+  },
+  {
+    "revision": "481ffe5312ee2dea724f40d7cff00821",
+    "url": "/React-js/static/media/808.481ffe53.gif"
+  },
+  {
+    "revision": "d93090e6b820f8a3de57ca40b03470b1",
+    "url": "/React-js/static/media/user.d93090e6.png"
+  }
+]);
